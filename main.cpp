2625111a3f0c2f9d538c22ac7f5166c7f8aa0441
na <k023g1066@g.neec.ac.jp>
2024-04-26 15:17:30 +0900

    00-01を作った

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Novice.h>
-#include "mathVector3.h"
+#include"mathVector3.h"
 
 const char kWindowTitle[] = "GC2B_12_ナカムラ_アヤネ";
 
@@ -7,12 +7,13 @@ const char kWindowTitle[] = "GC2B_12_ナカムラ_アヤネ";
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, 580, 500);
 
-	Vector3 v1{ 1.0f,3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
-	int kRowHeight = 50;
+	Matrix4x4 m1 = { 3.2f,0.7f,9.6f,4.4f,5.5f,1.3f,7.8f,2.1f,
+					 6.9f,8.0f,2.6f,1.0f,0.5f,7.2f,5.1f,3.3f };
+
+	Matrix4x4 m2 = { 4.1f,6.5f,3.3f,2.2f,8.8f,0.6f,9.9f,7.7f,
+					 1.1f,5.5f,6.6f,0.0f,3.3f,9.9f,8.8f,2.2f };
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -31,13 +32,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1, v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Nomalize(v2);
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -46,12 +40,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, resultAdd, " : Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
-		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
+		Matrix4x4 resultAdd = Add(m1, m2);
+		Matrix4x4 resultSubtract = Subtract(m1, m2);
+		Matrix4x4 resultMultiply = Multiply(m1, m2);
+		Matrix4x4 inverseM1 = Inverse(m1);
+		Matrix4x4 inverseM2 = Inverse(m2);
+		Matrix4x4 transposeM1 = Transpose(m1);
+		Matrix4x4 transposeM2 = Transpose(m2);
+		Matrix4x4 identity = MakeIndentity4x4();
+
+		MatrixScreenPrintf(0, 0, resultAdd/*"Add"*/);
+		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract/*, "Subtract"*/);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply/*, "Multiply"*/);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1/*, "inverseM1"*/);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2/*, "inverseM2"*/);
+		MatrixScreenPrintf(kColumWidth * 5, 0, transposeM1/*, "transposeM1"*/);
+		MatrixScreenPrintf(kColumWidth * 5, kRowHeight * 5, transposeM2/*, "transposeM2"*/);
+		MatrixScreenPrintf(kColumWidth * 5, kRowHeight * 5 * 2, identity/*, "identity"*/);
 
 		///
 		/// ↑描画処理ここまで
--- mathVector3.h
+++ mathVector3.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "Vector3.h"
+#include "Matrix4x4.h"
 
 //加算
 Vector3 Add(const Vector3& v1, const Vector3& v2);
@@ -15,3 +16,20 @@ float Length(const Vector3& v);
 Vector3 Nomalize(const Vector3& v);
 //表示関数
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+
+//1.行列の加法
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
+//2.行列の減法
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+//3.行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+//4.逆行列
+Matrix4x4 Inverse(const Matrix4x4& m);
+//5.転置行列
+Matrix4x4 Transpose(const Matrix4x4& m);
+//6.単位行列の作成
+Matrix4x4 MakeIndentity4x4();
+//表示関数
+static const int kRowHeight = 20;
+static const int kColumWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix);
